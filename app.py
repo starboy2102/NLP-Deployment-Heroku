3cc652a967fb99cf3521bbd42093e2efd8035fc4
Shivam Shrivastava <62115066+starboy2102@users.noreply.github.com>
2020-10-28 12:37:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNBimport joblib
-import pickle
+ import pickle
 
 # load the model from disk
 filename = 'nlp_model.pkl'
